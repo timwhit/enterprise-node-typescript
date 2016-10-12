339f2c76d26bfc6236b33f60273fff8c4dfe2db5
Tim Whitney <timw@slalom.com>
2016-10-12 15:47:52 -0500

    optimized imports.

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,16 @@
 import * as express from 'express';
-import * as winston from 'winston';
+import { Logger, LoggerInstance, LoggerOptions } from 'winston';
 import {AddressService, AddressServiceImpl} from './service/AddressService';
 
 const app: express.Application = express();
 const addressService: AddressService = new AddressServiceImpl();
 
-const logger: winston.LoggerInstance = new (winston.Logger)({exitOnError: false});
+const logger: LoggerInstance = new Logger(<LoggerOptions> {exitOnError: false});
 
 app.get('/', function (req: express.Request, res: express.Response) {
     res.send(addressService.getAddresses());
 });
 
 app.listen(3000, function () {
-    logger.error('Example app listening on port 3000!');
+    logger.info('', 'Example app listening on port 3000!');
 });
