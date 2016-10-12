7cee09e2cca8a522a38432c0cd0adea4ca226e56
Tim Whitney <timw@slalom.com>
2016-10-12 16:16:12 -0500

    fixed logging setup

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,21 @@
 import * as express from 'express';
-import { Logger, LoggerInstance, LoggerOptions } from 'winston';
+import {Logger, LoggerInstance, LoggerOptions, transports} from 'winston';
 import {AddressService, AddressServiceImpl} from './service/AddressService';
 
 const app: express.Application = express();
 const addressService: AddressService = new AddressServiceImpl();
 
-const logger: LoggerInstance = new Logger(<LoggerOptions> {exitOnError: false});
+const logger: LoggerInstance = new Logger(<LoggerOptions> {
+    exitOnError: false,
+    transports: [
+        new transports.Console()
+    ]
+});
 
 app.get('/', function (req: express.Request, res: express.Response) {
     res.send(addressService.getAddresses());
 });
 
 app.listen(3000, function () {
-    logger.info('', 'Example app listening on port 3000!');
+    logger.info('Example app listening on port 3000!');
 });
